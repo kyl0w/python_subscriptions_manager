01adc6984a853ca5fe84bb2bc573bada3fe99446
Tiago Lourenco <lourenco123tiago@gmail.com>
2025-01-08 14:51:08 +0000

    UI

--- views/view.py
+++ views/view.py
@@ -1,9 +1,9 @@
 import __init__
-
+import matplotlib.pyplot as plt
 from models.database import engine
 from models.model import Subscription, Payments
 from sqlmodel import Session, select
-from datetime import date
+from datetime import date, datetime
 
 class SubscriptionService:
     def __init__(self, engine):
@@ -44,6 +44,47 @@ class SubscriptionService:
 
         return float(total)
 
-ss = SubscriptionService(engine)
-# subscription = Subscription(empresa='netflix', site='netflix.com', data_assinatura=date.today(), valor=25)
-print(ss.list_all())
+    def total_value(self):
+        pass
+
+    def _get_last_12_months_active(self):
+        today = datetime.now()
+        year = today.year
+        month = today.month
+        last_12_month = []
+        for _ in range(12):
+            last_12_month.append((month, year))
+            month -= 1
+            if month == 0:
+                month = 12
+                year -= 1
+            
+        return last_12_month[::-1]
+    
+    def _get_values_for_months(self, last_12_months):
+        with Session(self.engine) as session:
+            statement = select(Payments)
+            results = session.exec(statement).all()
+
+            value_for_months = []
+            for i in last_12_months:
+                value = 0
+                for result in results:
+                    if result.date.month == i[0] and result.date.year == i[1]:
+                        value += float(result.subscription.valor)
+                value_for_months.append(value)
+            return value_for_months
+        
+    def gen_chart(self):
+        last_12_months = self._get_last_12_months_active()
+        value_for_months = self._get_values_for_months(last_12_months)
+
+        last_12_months = list(map(lambda x: x[0], self._get_last_12_months_active()))
+
+        plt.plot(range(len(last_12_months)), value_for_months, marker="o")
+        plt.xticks(ticks=range(len(last_12_months)), labels=last_12_months)
+        plt.xlabel("Months")
+        plt.ylabel("Values")
+        plt.title("Values Over the Last 12 Months")
+        plt.grid(True)
+        plt.show()
